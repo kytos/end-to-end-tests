1e5298c607c0eb50fdb67090984b4c16d15fe97f
Italo Valcy <italovalcy@gmail.com>
2020-06-25 20:22:08 -0400

    fixing errors on the tests and making kytos-init.sh agnostic to run on gitlab or docker-compose

--- kytos-init.sh
+++ kytos-init.sh
@@ -9,7 +9,7 @@ for napp in storehouse of_core flow_manager topology of_lldp pathfinder mef_elin
 
 apt-get update && apt-get install -y python-pytest python-requests python-mock python-pytest-timeout
 
-python -m pytest --timeout=600 tests/
+python -m pytest --timeout=60 tests/
 
 # leave tail running unless it is from Gitlab-CI
 [ -z "$CI_PROJECT_ID" ] && tail -f /dev/null
