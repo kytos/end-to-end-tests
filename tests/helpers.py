8880e700859249450c68c9353b636e7d5035bf52
Daniela Garcia <daniela@amlight.net>
2020-09-01 06:21:40 +0000

    Add class DanielaTopo to helpers.py since that is the topology I've used for my tests so far

--- tests/helpers.py
+++ tests/helpers.py
@@ -33,6 +33,37 @@ class RingTopo( Topo ):
         self.addLink( s2, s3 )
         self.addLink( s3, s1 )
 
+class DanielaTopo( Topo ):
+    """Create a network from semi-scratch with multiple controllers."""
+
+    def build(self):
+
+        #("*** Creating switches\n")
+        s1 = self.addSwitch('s1', listenPort=6601, dpid="1")
+        s2 = self.addSwitch('s2', listenPort=6602, dpid="2")
+        s3 = self.addSwitch('s3', listenPort=6603, dpid="3")
+        s4 = self.addSwitch('s4', listenPort=6604, dpid="4")
+
+        #("*** Creating hosts\n")
+        hosts1 = [self.addHost('h%d' % n) for n in (1, 2)]
+        hosts2 = [self.addHost('h%d' % n) for n in (3, 4)]
+        hosts3 = [self.addHost('h%d' % n) for n in (5, 6)]
+        hosts4 = [self.addHost('h%d' % n) for n in (7, 8)]
+
+        #("*** Creating links\n")
+        for h in hosts1:
+            self.addLink(s1, h)
+        for h in hosts2:
+            self.addLink(s2, h)
+        self.addLink(s1, s2)
+        self.addLink(s2, s3)
+        for h in hosts3:
+            self.addLink(s3, h)
+        for h in hosts4:
+            self.addLink(s4, h)
+        self.addLink(s3, s4)
+        self.addLink(s4, s1)
+
 class NetworkTest():
     def __init__(self, controller_ip):
         # Create an instance of our topology
