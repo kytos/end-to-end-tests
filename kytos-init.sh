b13682e349ba557c71b345dad99275516d6ef9a1
Daniela Garcia <daniela@amlight.net>
2020-09-01 06:22:53 +0000

    Update kytos-init.sh by adding an option to only run a specific test instead of all the tests.

--- kytos-init.sh
+++ kytos-init.sh
@@ -11,5 +11,8 @@ apt-get update && apt-get install -y python-pytest python-requests python-mock p
 
 python -m pytest --timeout=60 tests/
 
+#only runs specific test
+#python -m pytest --timeout=60 tests/test_e2e_10_mef_eline.py::TestE2EMefEline::test_on_primary_path_fail_should_migrate_to_backup
+
 # leave tail running unless it is from Gitlab-CI
 [ -z "$CI_PROJECT_ID" ] && tail -f /dev/null
