322a602f52139aa0c299f2acc4cf875934238923
Humberto Diógenes <hdiogenes@gmail.com>
2020-10-29 12:34:29 -0300

    Review formatting

--- kytos-init.sh
+++ kytos-init.sh
@@ -5,14 +5,19 @@ set -x
 service syslog-ng start
 service openvswitch-switch start
 
-for napp in storehouse of_core flow_manager topology of_lldp pathfinder mef_eline maintenance; do git clone https://github.com/kytos/$napp;  cd $napp; python3.6 setup.py develop || true; cd ..; done
+for napp in storehouse of_core flow_manager topology of_lldp pathfinder \
+            mef_eline maintenance; do
+    git clone https://github.com/kytos/$napp
+    (cd $napp; python3.6 setup.py develop || true)
+done
 
-apt-get update && apt-get install -y python-pytest python-requests python-mock python-pytest-timeout
+apt-get update
+apt-get install -y python-pytest python-requests python-mock python-pytest-timeout
 
 python -m pytest --timeout=60 tests/
 
-#only runs specific test
-#python -m pytest --timeout=60 tests/test_e2e_10_mef_eline.py::TestE2EMefEline::test_on_primary_path_fail_should_migrate_to_backup
+# only run specific test
+# python -m pytest --timeout=60 tests/test_e2e_10_mef_eline.py::TestE2EMefEline::test_on_primary_path_fail_should_migrate_to_backup
 
 # leave tail running unless it is from Gitlab-CI
 [ -z "$CI_PROJECT_ID" ] && tail -f /dev/null
