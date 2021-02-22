a87f743d4b6bda04b604d6fb22b790ae498e074c
Italo Valcy <italovalcy@gmail.com>
2021-02-22 15:21:02 -0500

    removing unecessary blank lines

--- tests/helpers.py
+++ tests/helpers.py
@@ -35,21 +35,17 @@ class RingTopo( Topo ):
 
 class DanielaTopo( Topo ):
     """Create a network from semi-scratch with multiple controllers."""
-
     def build(self):
-
         #("*** Creating switches\n")
         s1 = self.addSwitch('s1', listenPort=6601, dpid="1")
         s2 = self.addSwitch('s2', listenPort=6602, dpid="2")
         s3 = self.addSwitch('s3', listenPort=6603, dpid="3")
         s4 = self.addSwitch('s4', listenPort=6604, dpid="4")
-
         #("*** Creating hosts\n")
         hosts1 = [self.addHost('h%d' % n) for n in (1, 2)]
         hosts2 = [self.addHost('h%d' % n) for n in (3, 4)]
         hosts3 = [self.addHost('h%d' % n) for n in (5, 6)]
         hosts4 = [self.addHost('h%d' % n) for n in (7, 8)]
-
         #("*** Creating links\n")
         for h in hosts1:
             self.addLink(s1, h)
