9e6fe9410722fd5af5c7a170b71340dddf41a059
Arturo Quintana <arturocu81@gmail.com>
2021-06-14 16:24:16 -0400

    Renaming the flow_manager tests following logic in its content linked to its creation time - This commit retakes test_e2e_20_flow_manager.py as one of the initial files on the flow_manage testing process enclosing main tests.

--- tests/test_e2e_20_flow_manager.py
+++ tests/test_e2e_20_flow_manager.py
@@ -32,7 +32,7 @@ class TestE2EFlowManager:
     def teardown_class(cls):
         cls.net.stop()
 
-    def test_010_install_flow(self):
+    def test_005_install_flow(self):
         """Test if, after kytos restart, a flow installed
         to a switch will still be installed."""
 
@@ -76,7 +76,7 @@ class TestE2EFlowManager:
         assert len(flows_s1.split('\r\n ')) == 2
         assert 'actions=output:"s1-eth2"' in flows_s1
 
-    def test_011_install_flow_and_retrieve_it_back(self):
+    def test_010_install_flow_and_retrieve_it_back(self):
         """Test the flow status through the
         API's call after its installation."""
 
@@ -104,7 +104,7 @@ class TestE2EFlowManager:
         # It installs the flow
         api_url = KYTOS_API + '/flow_manager/v2/flows/' + switch_id
         requests.post(api_url, data=json.dumps(payload),
-                                 headers={'Content-type': 'application/json'})
+                      headers={'Content-type': 'application/json'})
 
         # wait for the flow to be installed
         time.sleep(10)
@@ -116,7 +116,7 @@ class TestE2EFlowManager:
         time.sleep(10)
 
         response = requests.get(api_url, data=json.dumps(payload),
-                                 headers={'Content-type': 'application/json'})
+                                headers={'Content-type': 'application/json'})
         assert response.status_code == 200
         data = response.json()
         assert len(data[switch_id]["flows"]) == 2
@@ -125,129 +125,6 @@ class TestE2EFlowManager:
         assert data[switch_id]["flows"][1]["priority"] == payload["flows"][0]["priority"]
         assert data[switch_id]["flows"][1]["idle_timeout"] == payload["flows"][0]["idle_timeout"]
         assert data[switch_id]["flows"][1]["hard_timeout"] == payload["flows"][0]["hard_timeout"]
-
-    def test_012_install_flow_on_non_existent_switch_should_fail(self):
-        """Test if the flow installation process on an
-        invalid switch behaves as expected (404 Error)."""
-
-        payload = {
-            "flows": [
-                {
-                    "priority": 10,
-                    "idle_timeout": 360,
-                    "hard_timeout": 1200,
-                    "match": {
-                        "in_port": 1
-                    },
-                    "actions": [
-                        {
-                            "action_type": "output",
-                            "port": 2
-                        }
-                    ]
-                }
-            ]
-        }
-
-        api_url = KYTOS_API + '/flow_manager/v2/flows/00:00:00:00:00:00:00:05'
-        response = requests.post(api_url, data=json.dumps(payload),
-                                 headers={'Content-type': 'application/json'})
-        assert response.status_code == 404
-
-    """
-    The Api call is returning 200 when should be 400
-    Issue https://github.com/kytos/flow_manager/issues/134
-    """
-    @pytest.mark.xfail
-    def test_0131_install_flow_should_fail(self):
-        """Test if the flow installation process specifying an empty
-        payload behaves as expected (400 Error)."""
-
-        payload = {}
-
-        api_url = KYTOS_API + '/flow_manager/v2/flows/00:00:00:00:00:00:00:01'
-        response = requests.post(api_url, data=json.dumps(payload),
-                                 headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
-
-    """
-    The Api call is returning 200 when should be 400
-    Issue https://github.com/kytos/flow_manager/issues/134
-    """
-    @pytest.mark.xfail
-    def test_0132_install_flow_should_fail(self):
-        """Test if the flow installation process specifying an empty
-        flow data on the payload behaves as expected (400 Error)."""
-
-        payload = {
-            "flows": [
-            ]
-        }
-
-        api_url = KYTOS_API + '/flow_manager/v2/flows/00:00:00:00:00:00:00:01'
-        response = requests.post(api_url, data=json.dumps(payload),
-                                 headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
-
-    """
-    The Api call is returning 200 when should be 400
-    Issue https://github.com/kytos/flow_manager/issues/134
-    """
-    @pytest.mark.xfail
-    def test_0133_install_flow_should_fail(self):
-        """Test if the flow installation process specifying an empty
-        flow data on the payload behaves as expected (400 Error)."""
-
-        payload = {
-            "flows": [
-                {
-
-                }
-            ]
-        }
-
-        api_url = KYTOS_API + '/flow_manager/v2/flows/00:00:00:00:00:00:00:01'
-        response = requests.post(api_url, data=json.dumps(payload),
-                                 headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
-
-    """
-    The execution breaks when should be returning a 400
-    Issue https://github.com/kytos/flow_manager/issues/134
-    """
-    @pytest.mark.xfail
-    def test_0134_install_flow_should_fail(self):
-        """Test if the flow installation process specifying a
-        wrong datatype payload behaves as expected (400 Error)."""
-
-        payload = {
-            "flows": [
-                {
-                    "priority"
-                }
-            ]
-        }
-
-        api_url = KYTOS_API + '/flow_manager/v2/flows/00:00:00:00:00:00:00:01'
-        response = requests.post(api_url, data=json.dumps(payload),
-                                 headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
-
-    """
-    The Api call is returning 500 when should be 404
-    Issue https://github.com/kytos/flow_manager/issues/131
-    """
-    @pytest.mark.xfail
-    def test_014_retrieve_flow_from_non_existent_switch_should_fail(self):
-        """Test if the flow retrieving process of an invalid
-        switch behaves as expected (404 Error)."""
-
-        switch_id = '00:00:00:00:00:00:00:05'
-
-        # It tries to get a flow that does not exist
-        api_url = KYTOS_API + '/flow_manager/v2/flows/' + switch_id
-        response = requests.get(api_url)
-        assert response.status_code == 404
 
     def test_015_install_flows(self):
         """Test if, after kytos restart, a flow installed to all switches will
@@ -293,85 +170,6 @@ class TestE2EFlowManager:
             flows_sw = sw.dpctl('dump-flows')
             assert len(flows_sw.split('\r\n ')) == 2
             assert 'actions=output:"%s-eth2"' % sw_name in flows_sw
-
-    """
-    The Api call is returning 200 when should be 400
-    Issue https://github.com/kytos/flow_manager/issues/132
-    """
-    @pytest.mark.xfail
-    def test_016_install_flows_should_fail(self):
-        """Test if the flow installation process specifying an empty
-        payload behaves as expected (400 Error)."""
-
-        payload = {}
-
-        api_url = KYTOS_API + '/flow_manager/v2/flows'
-        response = requests.post(api_url, data=json.dumps(payload),
-                                 headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
-
-    """
-    The Api call is returning 200 when should be 400
-    Issue https://github.com/kytos/flow_manager/issues/132
-    """
-    @pytest.mark.xfail
-    def test_017_install_flows_should_fail(self):
-        """Test if the flow installation process specifying an empty
-        flow data on the payload behaves as expected (400 Error)."""
-
-        payload = {
-            "flows": [
-            ]
-        }
-
-        api_url = KYTOS_API + '/flow_manager/v2/flows'
-        response = requests.post(api_url, data=json.dumps(payload),
-                                 headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
-
-    """
-    The Api call is returning 200 when should be 400
-    Issue https://github.com/kytos/flow_manager/issues/132
-    """
-    @pytest.mark.xfail
-    def test_018_install_flows_should_fail(self):
-        """Test if the flow installation process specifying an empty
-        flow data on the payload behaves as expected (400 Error)."""
-
-        payload = {
-            "flows": [
-                {
-
-                }
-            ]
-        }
-
-        api_url = KYTOS_API + '/flow_manager/v2/flows'
-        response = requests.post(api_url, data=json.dumps(payload),
-                                 headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
-
-    """
-    The Api call fails on runtime when should return a 400
-    Issue https://github.com/kytos/flow_manager/issues/132
-    """
-    @pytest.mark.xfail
-    def test_019_install_flows_should_fail(self):
-        """Test if the flow installation process specifying a
-        wrong datatype payload behaves as expected (400 Error)."""
-
-        payload = {
-            "flows": [
-                {
-                    "priority"
-                }
-            ]
-        }
-
-        api_url = KYTOS_API + '/flow_manager/v2/flows'
-        response = requests.post(api_url, data=json.dumps(payload),
-                                 headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
 
     def test_020_delete_flow(self):
         """Test if, after kytos restart, a flow deleted from a switch will
@@ -428,118 +226,6 @@ class TestE2EFlowManager:
         assert len(flows_s1.split('\r\n ')) == 1
         assert 'actions=output:"s1-eth2"' not in flows_s1
 
-    def test_021_delete_flow_on_non_existent_switch_should_fail(self):
-        """Test if the flow deletion process specifying an invalid
-        switch behaves as expected (404 Error)."""
-
-        payload = {
-            "flows": [
-                {
-                    "priority": 10,
-                    "idle_timeout": 360,
-                    "hard_timeout": 1200,
-                    "match": {
-                        "in_port": 1
-                    },
-                    "actions": [
-                        {
-                            "action_type": "output",
-                            "port": 2
-                        }
-                    ]
-                }
-            ]
-        }
-
-        # delete the flow
-        api_url = KYTOS_API + '/flow_manager/v2/flows/00:00:00:00:00:00:00:05'
-        response = requests.delete(api_url, data=json.dumps(payload),
-                                   headers={'Content-type': 'application/json'})
-        assert response.status_code == 404
-
-    """
-    The Api call is returning 200 when should be 400
-    Issue https://github.com/kytos/flow_manager/issues/136
-    """
-    @pytest.mark.xfail
-    def test_022_delete_flow_should_fail(self):
-        """Test if the flow deletion process specifying an empty
-        payload behaves as expected (400 Error)."""
-
-        payload = {}
-
-        # delete the flow
-        api_url = KYTOS_API + '/flow_manager/v2/flows/00:00:00:00:00:00:00:01'
-        response = requests.delete(api_url, data=json.dumps(payload),
-                                   headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
-
-    """
-    The Api call is returning 200 when should be 400
-    Issue https://github.com/kytos/flow_manager/issues/136
-    """
-    @pytest.mark.xfail
-    def test_023_delete_flow_should_fail(self):
-        """Test if the flow deletion process specifying an empty
-        flow data on the payload behaves as expected (400 Error)."""
-
-        payload = {
-            "flows": [
-            ]
-        }
-
-        # delete the flow
-        api_url = KYTOS_API + '/flow_manager/v2/flows/00:00:00:00:00:00:00:01'
-        response = requests.delete(api_url, data=json.dumps(payload),
-                                   headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
-
-    """
-    The Api call is returning 200 when should be 400
-    Issue https://github.com/kytos/flow_manager/issues/136
-    """
-    @pytest.mark.xfail
-    def test_024_delete_flow_should_fail(self):
-        """Test if the flow deletion process specifying an an empty
-        flow data on the payload behaves as expected (400 Error)."""
-
-        payload = {
-            "flows": [
-                {
-
-                }
-            ]
-        }
-
-        # delete the flow
-        api_url = KYTOS_API + '/flow_manager/v2/flows/00:00:00:00:00:00:00:01'
-        response = requests.delete(api_url, data=json.dumps(payload),
-                                   headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
-
-    """
-    The Api call fails on runtime when should return a 400
-    Issue https://github.com/kytos/flow_manager/issues/136
-    """
-    @pytest.mark.xfail
-    def test_0241_delete_flow_should_fail(self):
-        """Test if the flow deletion process specifying a
-        wrong datatype payload behaves as expected (400 Error)."""
-
-        payload = {
-            "flows": [
-                {
-                    "priority"
-                }
-            ]
-        }
-
-        # delete the flow
-        api_url = KYTOS_API + '/flow_manager/v2/flows/00:00:00:00:00:00:00:01'
-        response = requests.delete(api_url, data=json.dumps(payload),
-                                   headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
-
     def test_025_delete_flows(self):
         """Test if, after kytos restart, a flow deleted from all switches will
            still be deleted."""
@@ -595,89 +281,6 @@ class TestE2EFlowManager:
             flows_sw = sw.dpctl('dump-flows')
             assert len(flows_sw.split('\r\n ')) == 1
             assert 'actions=output:"%s-eth2"' % sw_name not in flows_sw
-
-    """
-    The Api call is returning 200 when should be 400
-    Issue https://github.com/kytos/flow_manager/issues/135
-    """
-    @pytest.mark.xfail
-    def test_026_delete_flows_should_fail(self):
-        """Test if the flow deletion process specifying an empty
-        payload behaves as expected (400 Error)."""
-
-        payload = {}
-
-        # delete the flow
-        api_url = KYTOS_API + '/flow_manager/v2/flows'
-        response = requests.delete(api_url, data=json.dumps(payload),
-                                   headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
-
-    """
-    The Api call is returning 200 when should be 400
-    Issue https://github.com/kytos/flow_manager/issues/135
-    """
-    @pytest.mark.xfail
-    def test_027_delete_flows_should_fail(self):
-        """Test if the flow deletion process specifying an empty
-        flow data on the payload behaves as expected (400 Error)."""
-
-        payload = {
-            "flows": [
-            ]
-        }
-
-        # delete the flow
-        api_url = KYTOS_API + '/flow_manager/v2/flows'
-        response = requests.delete(api_url, data=json.dumps(payload),
-                                   headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
-
-    """
-    The Api call is returning 200 when should be 400
-    Issue https://github.com/kytos/flow_manager/issues/135
-    """
-    @pytest.mark.xfail
-    def test_028_delete_flows_should_fail(self):
-        """Test if the flow deletion process specifying an empty
-        flow data on the payload behaves as expected (400 Error)."""
-
-        payload = {
-            "flows": [
-                {
-
-                }
-            ]
-        }
-
-        # delete the flow
-        api_url = KYTOS_API + '/flow_manager/v2/flows'
-        response = requests.delete(api_url, data=json.dumps(payload),
-                                   headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
-
-    """
-    The Api call fails on runtime when should return a 400
-    Issue https://github.com/kytos/flow_manager/issues/135
-    """
-    @pytest.mark.xfail
-    def test_029_delete_flows_should_fail(self):
-        """Test if the flow deletion process specifying a
-        wrong datatype payload behaves as expected (400 Error)."""
-
-        payload = {
-            "flows": [
-                {
-                    "priority"
-                }
-            ]
-        }
-
-        # delete the flow
-        api_url = KYTOS_API + '/flow_manager/v2/flows'
-        response = requests.delete(api_url, data=json.dumps(payload),
-                                   headers={'Content-type': 'application/json'})
-        assert response.status_code == 400
 
     def modify_match(self, restart_kytos=False):
         """Test if after a match is modified outside kytos, the original
