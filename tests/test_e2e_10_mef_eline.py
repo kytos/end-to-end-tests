7b6488dba0cbcf6d816627ac5d196d92df11f2aa
Arturo Quintana <arturocu81@gmail.com>
2021-08-04 16:43:55 -0400

    Specification of more accurate information to enforce the attributes correlation and the testing of the primary_links patching process

--- tests/test_e2e_10_mef_eline.py
+++ tests/test_e2e_10_mef_eline.py
@@ -1195,7 +1195,7 @@ class TestE2EMefEline:
 
         time.sleep(10)
 
-        payload = {
+        payload2 = {
             "primary_links": [
                 {"endpoint_a": {"id": "00:00:00:00:00:00:00:01:3"},
                  "endpoint_b": {"id": "00:00:00:00:00:00:00:02:2"}},
@@ -1205,7 +1205,7 @@ class TestE2EMefEline:
         }
 
         # It sets a new circuit's backup_links
-        response = requests.patch(api_url + evc1, data=json.dumps(payload),
+        response = requests.patch(api_url + evc1, data=json.dumps(payload2),
                                   headers={'Content-type': 'application/json'})
         # print(response)
         assert response.status_code == 200
@@ -1215,8 +1215,14 @@ class TestE2EMefEline:
         # It verifies EVC's data
         response = requests.get(api_url + evc1)
         data = response.json()
-        assert data['primary_links'][0]['endpoint_a'] == payload['primary_links'][0]['endpoint_a']
-        assert data['primary_links'][0]['endpoint_b'] == payload['primary_links'][0]['endpoint_b']
+
+        paths = []
+        for _path in data['primary_links']:
+            paths.append({"endpoint_a": {"id": _path['endpoint_a']['id']},
+                          "endpoint_b": {"id": _path['endpoint_b']['id']}})
+
+        assert paths == payload2["primary_links"]
+        assert data['active'] is True
 
     def test_155_current_path_value_given_dynamic_backup_path_and_primary_path_conditions(self):
         payload = {
