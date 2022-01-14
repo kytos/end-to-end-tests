bc3c87136aefd9b955da479b4507dada1ba3cb8b
Italo Valcy <italo@ampath.net>
2022-01-14 07:16:13 -0300

    start/stop times should apply only for failed tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,7 @@ def pytest_terminal_summary(terminalreporter):
     terminalreporter.section('start/stop times', sep='-', bold=True)
     for stat in terminalreporter.stats.values():
         for report in stat:
-            if report.when == 'call' and report.failed and hasattr(report, 'start') and hasattr(report, 'stop'):
+            if hasattr(report, 'failed') and report.failed and report.when == 'call':
                 start = datetime.fromtimestamp(report.start)
                 stop = datetime.fromtimestamp(report.stop)
                 terminalreporter.write_line('{id:20}: {start:%Y-%m-%d,%H:%M:%S.%f} - {stop:%Y-%m-%d,%H:%M:%S.%f}'.format(id=report.nodeid, start=start, stop=stop))
